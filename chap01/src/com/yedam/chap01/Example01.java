90c675e14ca1df849237462691705fdccbc021cd
admin <admin@YD02-08>
2023-05-08 15:27:21 +0900

    first Commit

---
+++ chap01/src/com/yedam/chap01/Example01.java
@@ -0,0 +1,16 @@
+package com.yedam.chap01;
+// 패키지 com밑에 yedam밑에 chp01
+public class Example01 {
+//클래스 파일명
+	public static void main(String[] args) {//java는 main method에서 실행시작 public static void main(String[] args)로 시작함
+		System.out.println("Hello World");//이 한줄을 실행문이라 부름 작성 후 세미콜론 붙여야함
+//	//함수=메소드
+		//한줄주석
+		/*
+		*->여러줄 주석
+		*
+		*/ 
+		//-> 실행시 주석은 class 파일에 포함x 실행시켜도 포함안됨.
+	}
+}
+//
